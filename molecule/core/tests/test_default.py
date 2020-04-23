e0266f1f8fc92bff392068f9563e93c6250b27b2
David Brown <dmlb2000@gmail.com>
2020-04-22 21:47:48 -0700

    Refactor all the Testing (#24)
    This refactors the molecule testing to only perform one platform
    for one scenario at a time.
    
    The gen-scenarios.py script auto generates the converge and
    molecule configuration for each scenario.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- molecule/core/tests/test_default.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""Default testing for pacifica virtualenv."""
-
-
-def test_pacifica_virtualenv(host):
-    """Test the pacifica virtualenv."""
-    run_file = host.file('/opt/default/bin/activate')
-    assert run_file.exists
